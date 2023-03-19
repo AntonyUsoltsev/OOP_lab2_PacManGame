db344088bbbc41265d233acca57c3716286ce315
AntonyUsoltsev <usoltsev_anton1@mail.ru>
2023-03-19 17:12:02 +0700

    first fx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module nsu.usoltsev.pacmangame {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens nsu.usoltsev.pacmangame to javafx.fxml;
+    exports nsu.usoltsev.pacmangame;
+}
